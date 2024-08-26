46008e8f04e2fd83b019d8ae4bd69d24a67f0931
dmoskalev <dmoskalev@gmail.com>
2024-08-26 22:42:50 +0300

    Changed 404 page

--- src/components/not-found.tsx
+++ src/components/not-found.tsx
@@ -12,7 +12,7 @@ export function NotFound() {
                 <Separator className='my-10'/>
                 <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-8'>
                     We're sorry<br/>
-                    Your link not found
+                    Your link not found or expired
                 </h1>
                 <a href={`${CONFIG.baseDir}/`} className={buttonVariants({ variant: 'secondary' })}>
                     Create another link
