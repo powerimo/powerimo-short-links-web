6278094e258802aa13588b4f754072eb6a037efa
dmoskalev <dmoskalev@gmail.com>
2024-08-26 23:32:04 +0300

    Changed 404 page

--- src/components/not-found.tsx
+++ src/components/not-found.tsx
@@ -6,11 +6,11 @@ export function NotFound() {
     return (
         <div className='flex-1 container content-center px-4 md:px-6'>
             <div className='w-full max-w-lg m-auto'>
-                <h2 className='scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0'>
+                <h3 className='scroll-m-20 text-2xl font-semibold tracking-tight first:mt-0'>
                     Error 404
-                </h2>
+                </h3>
                 <Separator className='my-10'/>
-                <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-8'>
+                <h1 className='scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl mb-8'>
                     We're sorry<br/>
                     Your link not found or expired
                 </h1>
