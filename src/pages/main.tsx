84e51974dea9212422d962e1f76570b9d5ef7042
andewil <voronin@andewil.com>
2025-01-13 06:53:34 +0100

    Empty password has to be null

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -59,7 +59,7 @@ export function Main() {
                 secret: data.secretText,
                 hitLimit: data.hitLimit,
                 ttl,
-                password: data.password,
+                password: data.password && data.password != "" ? data.password : null,
             }),
         });
         if (response.ok) {
