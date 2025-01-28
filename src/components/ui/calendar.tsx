d253a18731a02ac160fc406cef45eb80e8f2e445
Dmitrii Moskalev <dmoskalev@gmail.com>
2025-01-28 21:00:17 +0300

    Added some languages translated from English by ChatGPT Added calendar translation

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,15 +1,20 @@
 import * as React from 'react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { DayPicker } from 'react-day-picker';
+import * as locale from 'date-fns/locale';
 
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
+import { useTranslation } from 'react-i18next';
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
 function Calendar({ className, classNames, showOutsideDays = true, ...props }: CalendarProps) {
+    const { i18n } = useTranslation();
+
     return (
         <DayPicker
+            locale={locale[i18n.language as keyof typeof locale]}
             showOutsideDays={showOutsideDays}
             className={cn('p-3', className)}
             classNames={{
